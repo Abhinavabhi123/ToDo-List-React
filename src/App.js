7eb0c14d5004ea25008393559139dc4bd50a5f2b
Abhinav <abhinavkinarullathil@gmail.com>
2023-05-11 17:17:56 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React  from 'react'
+import './App.css';
+import TodoForm from './component/TodoForm';
+
+function App() {
+return(
+   <div className='todo-app'>
+    <TodoForm/>
+   </div>
+  );
+}
+
+export default App;
