46d70cbb87855a36c764ded45ff128a61fccf950
Abhinav <abhinavkinarullathil@gmail.com>
2023-05-12 17:41:08 +0530

    designing and functionality added

--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -3,15 +3,29 @@ import TodoForm from './TodoForm';
 import Todo from './Todo';
 
 function TodoList() {
-    const [todos,setTodos]=useState([ ])
-    const addTodo =todo=>{
-        if(!todo.text || /^\s*$/.test(todo.text)){
-            return
-        }
-        const newTodos =[todo,...todos];
-        setTodos(newTodos);
-        console.log(...todos);
+  const [todos,setTodos]=useState([ ])
+  const addTodo =todo=>{
+      if(!todo.text || /^\s*$/.test(todo.text)){
+          return
+      }
+      const newTodos =[todo,...todos];
+      setTodos(newTodos);
+      console.log(...todos);
+  }
+ 
+  const updateTodo =(todoId,newValue)=>{
+    if(!newValue.text || /^\s*$/.test(newValue.text)){
+      return
     }
+    setTodos(prev=>prev.map(item=>(item.id === todoId ? newValue:item)))
+  }
+
+  const removeTodo =id=>{
+    const removeArr =[...todos.filter(todo=>todo.id !== id)]
+
+    setTodos(removeArr)
+  }
+
 
   const completeTodo=id=>{
     let updatedTodo = todos.map(todo => { 
@@ -26,7 +40,11 @@ function TodoList() {
     <div>
       <h1>What is the plan for Today!</h1>
       <TodoForm onSubmit={addTodo}/>
-      <Todo todos={todos} completeTodo={completeTodo}/>
+      <Todo 
+      todos={todos} 
+      completeTodo={completeTodo} 
+      removeTodo={removeTodo}
+      updateTodo={updateTodo}/>
     </div>
   )
 }
