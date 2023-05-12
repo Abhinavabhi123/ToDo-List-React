82df07de0a1009559a937e9f15564e29e50e381e
Abhinav <abhinavkinarullathil@gmail.com>
2023-05-12 14:54:21 +0530

    saved todo contents and listed all tasks in the todo

--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -1,20 +1,32 @@
 import React,{useState} from 'react';
 import TodoForm from './TodoForm';
+import Todo from './Todo';
 
 function TodoList() {
-    const [todos,setTodos]=useState([])
-const addTodo =todo=>{
-    if(!todo.text || /^\s*$/.test(todo.text)){
-        return
+    const [todos,setTodos]=useState([ ])
+    const addTodo =todo=>{
+        if(!todo.text || /^\s*$/.test(todo.text)){
+            return
+        }
+        const newTodos =[todo,...todos];
+        setTodos(newTodos);
+        console.log(...todos);
     }
-    const newTodos =[todo,...todos];
-    setTodos(newTodos);
-    console.log(...todos);
-}
+
+  const completeTodo=id=>{
+    let updatedTodo = todos.map(todo => { 
+      if(todo.id === id){
+        todo.isComplete = !todo.isComplete
+      }
+      return todo
+    })
+    setTodos(updatedTodo)
+  }
   return (
     <div>
       <h1>What is the plan for Today!</h1>
       <TodoForm onSubmit={addTodo}/>
+      <Todo todos={todos} completeTodo={completeTodo}/>
     </div>
   )
 }
